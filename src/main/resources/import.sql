bb667d1f07381457dda0d4fe6794c64c58d6b583
Niel Eyde <nieleyde@gmail.com>
2017-06-01 14:18:35 -0400

    added fruit

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 insert into fruit (name) values ('Cherry');
 insert into fruit (name) values ('Apple');
 insert into fruit (name) values ('Banana');
+insert into fruit (name) values ('Mango');
+insert into fruit (name) values ('Blueberry');
+insert into fruit (name) values ('Orange');
+insert into fruit (name) values ('Grapefruit');
